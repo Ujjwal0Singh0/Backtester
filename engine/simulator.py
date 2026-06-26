7dcd51a051dcfef193fd77d5b2fe34f4f34f3670
Ananya <ananya24.garg@gmail.com>
2026-06-26 14:36:52 +0530

    for simulator.y done parts 1,4 and partially 5, 2 and 3 depends on machine_engine.py

--- engine/simulator.py
+++ engine/simulator.py
@@ -14,8 +14,14 @@ class Simulator:
     Orchestrates the sequence of Clock Tick, Order Evaluation, Fills & Settlement, 
     and Strategy Pulse.
     """
-    def __init__(self, event_bus: EventBus, tick_generator: TickGenerator, 
-                 matching_engine: MatchingEngine, portfolio: Portfolio, strategy: Any):
+    def __init__(
+        self,
+        event_bus: EventBus,
+        tick_generator: TickGenerator, 
+        matching_engine: MatchingEngine, 
+        portfolio: Portfolio, 
+        strategy: Any
+        ):
         """
         Initializes the Simulator.
         
@@ -26,7 +32,12 @@ class Simulator:
             portfolio (Portfolio): The active ledger.
             strategy (Any): The trading strategy to execute.
         """
-        pass
+
+        self.event_bus = event_bus
+        self.tick_generator = tick_generator
+        self.matching_engine = matching_engine
+        self.portfolio = portfolio
+        self.strategy = strategy
 
     def run(self, symbol: str, start_date: str, end_date: str) -> None:
         """
@@ -37,4 +48,50 @@ class Simulator:
             start_date (str): The start date.
             end_date (str): The end date.
         """
-        raise NotImplementedError
+        tick_stream = self.tick_generator.generate_ticks(
+            symbol=symbol,
+            start_date=start_date,
+            end_date=end_date,
+        )
+        # Main chronological loop
+        for tick in tick_stream:
+
+            # Every market tick enters the EventBus first
+            self.event_bus.publish(tick)
+
+            # Process every pending event in FIFO order
+            while self.event_bus.has_events():
+
+                event = self.event_bus.get_next_event()
+
+                if event is None:
+                    continue
+
+                # Tick Events
+                if isinstance(event, TickEvent):
+
+                    # Update unrealized PnL.
+                    self.portfolio.mark_to_market(event)
+
+                    # Notify the trading strategy.
+                    # Strategy may generate new OrderEvents.
+                    self.strategy.on_ticks(None, [event])
+
+                    # TODO:
+                    # self.matching_engine.evaluate_ticks(event)
+
+
+                # Order Events
+                elif isinstance(event, dict):
+
+                    if event.get("event_type") == "ORDER":
+
+                        # TODO:
+                        # self.matching_engine.process_order(event)
+                        pass
+
+                    elif event.get("event_type") == "FILL":
+
+                        # TODO:
+                        # self.portfolio.update_from_fill(event)
+                        pass
